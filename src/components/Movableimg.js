395302c2d9cc0b72f281e0194cd3bcd3039aa315
Siddhaant Pahuja <98097369+serious22@users.noreply.github.com>
2023-02-28 16:53:59 +0530

    Update Movableimg.js

--- src/components/Movableimg.js
+++ src/components/Movableimg.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { useDraggable } from '@dnd-kit/core';
+import {CSS} from '@dnd-kit/utilities';
 
-export default function Movableimg({id,src,atext}) {
-    const { isDragging, attributes, setNodeRef } = useDraggable({
-        id: id,
-        data: { type: 'image' }
+export default function Movableimg(props) {
+   
+    const { isDragging, attributes, setNodeRef, transform, listeners } = useDraggable({
+        id: props.id,
+        data: {
+            supports: ['images'],
+          },
+        item:{id: props.id},
     });
-
+    
+    const style = {
+        transform: CSS.Translate.toString(transform),
+        border: isDragging ? '5px solid pink' : 'none'
+      };
     return (
-        <img 
-            ref={setNodeRef} 
-            src={src} 
-            alt={atext}
-            style={{ border: isDragging ? '5px solid pink' : 'none' }}
-            {...attributes}
-        />
+        <>
+            
+            <img 
+                ref={setNodeRef} 
+                src={props.src} 
+                alt={props.atext}
+                style={style}
+                {...attributes}
+                {...listeners}
+            />
+        </>
     );
 }
