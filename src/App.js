e1ccfd432c86af6024db36fead2b1d07beb6b9ac
serious22 <siddhaant16.7@hotmail.com>
2023-03-01 21:56:04 +0530

    added changeable objects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from './components/Navbar'
 import Imgdrags from './components/Imgdrags'
-import { DndContext } from '@dnd-kit/core'
+import { DndContext, CollisionDetection ,closestCenter } from '@dnd-kit/core'
+import Changeobjects from './components/Changeobjects'
+import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
+
+
 
 export default function App() {
+  
+  const names = ["my name","is","Sid"];
+  const[name,setName] = useState(names);
   return (
     <>
       <Navbar/>
@@ -11,7 +18,27 @@ export default function App() {
       <DndContext>
         <Imgdrags/>
       </DndContext>
+      
+      <DndContext CollisionDetection={closestCenter} onDragEnd = {handledragend}>
+        <SortableContext items={name}
+        strategy = {verticalListSortingStrategy}>
+          {name.map(name => <Changeobjects key = {name} id ={name} />)}
+        </SortableContext>
+      </DndContext>
 
     </>
   )
+  function handledragend(event) {
+    const{active,over} = event;
+
+    if (active.id !== over.id) {  
+      setName((items)=> {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+        return arrayMove(items,activeIndex,overIndex);
+      })
+      
+    }
+    
+  }
 }
---
+++ src/components/Changeobjects.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities';
+import '../App.css';
+
+
+export default function Changeobjects(props) {
+    const{
+        attributes,
+        listeners,
+        setNodeRef,
+        transition,
+        transform,
+        isDragging
+    } = useSortable({id:props.id});
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+        opacity: isDragging ? 0.5:1
+    }
+  return (
+    <div className="changeContainer">
+        <div ref={setNodeRef} style={style} className ="mycard" {...attributes} {...listeners}>
+            {props.id}
+        </div>
+    </div>
+  )
+}
